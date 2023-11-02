dfa52710b01b160ac39260a4b70eaad2ff954774
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2023-11-02 12:04:32 -0600

    added mock data to front end

--- src/services/search-service.ts
+++ src/services/search-service.ts
@@ -1,4 +1,6 @@
 import { SearchRequest } from '@/models/search-request';
+import { SearchResponse } from '@/models/search-response';
+import { SearchResult } from '@/models/search-result';
 
 class SearchService {
   private baseUrl: string;
@@ -8,34 +10,55 @@ class SearchService {
     this.functionsKey = import.meta.env.VITE_API_KEY_CODE;
   }
 
-  getAll() {
-    return fetch(this.baseUrl + '/FlightReportAll', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-functions-key': this.functionsKey,
-      },
-    }).then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
+  getAll(): Promise<SearchResponse> {
+    return new Promise((resolve, reject) => {
+      return resolve({ searchResults: SampleData.GetSampleResults() });
     });
+
+    // return fetch(this.baseUrl + '/FlightReportAll', {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'x-functions-key': this.functionsKey,
+    //   },
+    // }).then((response) => {
+    //   if (!response.ok) {
+    //     throw new Error('Network response was not ok');
+    //   }
+    //   return response.json();
+    // });
   }
-  search(query: SearchRequest) {
-    return fetch(this.baseUrl + '/FlightReportSearch', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(query),
-    }).then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    });
-  }
+  //   search(query: SearchRequest) {
+  //     return fetch(this.baseUrl + '/FlightReportSearch', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(query),
+  //     }).then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
+  //       return response.json();
+  //     });
+  //   }
 }
 
+export class SampleData {
+  static GetSampleResults(): SearchResult[] {
+    const results: SearchResult[] = [];
+
+    for (let i = 1; i <= 50; i++) {
+      results.push({
+        vendor: `Vendor${i}`,
+        businessId: 200 + i,
+        contractId: 100 + i,
+        type: i % 2 == 0 ? 1 : 2,
+        numTimeReports: i,
+      });
+    }
+
+    return results;
+  }
+}
 export default new SearchService();
