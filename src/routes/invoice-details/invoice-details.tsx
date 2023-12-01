507c99721613eaf0c631cf9040fd62a22b0265a6
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2023-12-01 15:21:39 -0700

    layout internal containers

--- src/routes/invoice-details/invoice-details.tsx
+++ src/routes/invoice-details/invoice-details.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import styles from './invoice-details.module.scss';
 
-let { container, content, sidebar, main, footer } = styles;
+let { container, content, sideBar, main, footer } = styles;
 
 export default function InvoiceDetails() {
   const { invoiceId } = useParams();
@@ -9,7 +9,7 @@ export default function InvoiceDetails() {
   return (
     <div className={container}>
       <div className={content}>
-        <div className={sidebar}>sideBar</div>
+        <div className={sideBar}>sideBar</div>
         <div className={main}>main</div>
       </div>
       <div className={footer}>footer</div>
