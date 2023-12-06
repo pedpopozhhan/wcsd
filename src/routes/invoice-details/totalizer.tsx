23fc98e0a977d7d1a607e1ebbd5cecae689cefca
Viral.Patel <Viral.Patel@V-VD-PD-0-1402>
2023-12-06 10:58:03 -0500

    Invoice Totalizer Section

--- src/routes/invoice-details/totalizer.tsx
+++ src/routes/invoice-details/totalizer.tsx
@@ -1,6 +1,27 @@
-import styles from './summary.module.scss';
+import { GoAContainer, GoATable } from '@abgov/react-components';
+import styles from './totalizer.module.scss';
 
-let { container } = styles;
+let { totalizerAmount, totalizerAmountLabel } = styles;
+
 export default function Totalizer() {
-  return <div className={container}>Totalizer</div>;
+  return (
+    <GoAContainer>
+      <div  className={totalizerAmountLabel}>Invoice Amount</div>
+      <div>
+        <label className={totalizerAmount}>$27,000.00</label>
+      </div>
+      <br />
+      <div className={totalizerAmountLabel}>Reconciled Amount</div>
+      <div>
+        {' '}
+        <label className={totalizerAmount}>$0.00</label>
+      </div>
+      <br />
+      <div className={totalizerAmountLabel}>Remaining Amount</div>
+      <div>
+        {' '}
+        <label className={totalizerAmount}>$27,000.00</label>
+      </div>
+    </GoAContainer>
+  );
 }
