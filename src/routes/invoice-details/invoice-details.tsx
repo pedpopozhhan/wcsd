e62bf533f40f1a069e16c361f29ed7b6f5ffe6d8
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2023-12-06 15:10:11 -0700

    added tabs, more layout fixes for independent scrollbars

--- src/routes/invoice-details/invoice-details.tsx
+++ src/routes/invoice-details/invoice-details.tsx
@@ -3,21 +3,27 @@ import styles from './invoice-details.module.scss';
 import Summary from './summary';
 import Totalizer from './totalizer';
 import DetailsTable from './details-table';
+import { GoATab, GoATabs } from '@abgov/react-components';
 
-let { container, content, sideBar, main, footer } = styles;
+let { container, content, sideBar, main, footer, header, tabs } = styles;
 
 export default function InvoiceDetails() {
   const { invoiceId } = useParams();
-
+  const initialTab = 1;
   return (
     <div className={container}>
       <div className={content}>
         <div className={sideBar}>
+          <div className={header}>Invoice</div>
           <Totalizer />
           <Summary />
         </div>
         <div className={main}>
-          <DetailsTable />
+          <GoATabs initialTab={initialTab}>
+            <GoATab heading='Details'>
+              <DetailsTable />
+            </GoATab>
+          </GoATabs>
         </div>
       </div>
       <div className={footer}>footer</div>
