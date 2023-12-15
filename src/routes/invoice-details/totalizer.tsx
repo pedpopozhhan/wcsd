93d590c98d0cac4e799719ce109d619812ca1843
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2023-12-15 14:20:34 -0700

    added animation to invoice amounts

--- src/routes/invoice-details/totalizer.tsx
+++ src/routes/invoice-details/totalizer.tsx
@@ -1,16 +1,54 @@
-import { GoAContainer, GoATable } from '@abgov/react-components';
 import styles from './totalizer.module.scss';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { convertToCurrency } from '@/common/currency';
 
-let { container, totalizerAmount, totalizerAmountLabel, section } = styles;
+let {
+  container,
+  totalizerAmount,
+  totalizerAmountLabel,
+  section,
+  reconciledAmount,
+  remainingAmount,
+} = styles;
 interface ITotalizerProps {
   invoiceAmount: number;
   reconciledAmount: number;
   remainingAmount: number;
 }
-//   const DetailsTable: React.FC<IDetailsTableProps> = (props) =>
+
 const Totalizer: FC<ITotalizerProps> = (props) => {
+  const [reconciledAmountStyle, setReconciledAmountStyle] = useState({
+    '--recWhole': 0,
+    '--recFraction': 0,
+  });
+  const [remainingAmountStyle, setRemainingAmountStyle] = useState({
+    '--remWhole': 0,
+    '--remFraction': 0,
+  });
+
+  useEffect(() => {
+    // fix to 2 decimal places
+    const reconciled = (Math.round(props.reconciledAmount * 100) / 100).toFixed(
+      2
+    );
+    const reconciledSplits = reconciled.toString().split('.');
+    const recProperties = {
+      '--recWhole': parseInt(reconciledSplits[0]),
+      '--recFraction': parseInt(reconciledSplits[1]),
+    };
+
+    const remaining = (Math.round(props.remainingAmount * 100) / 100).toFixed(
+      2
+    );
+    const remainingSplits = remaining.toString().split('.');
+    const remProperties = {
+      '--remWhole': parseInt(remainingSplits[0]),
+      '--remFraction': parseInt(remainingSplits[1]),
+    };
+    setRemainingAmountStyle(remProperties);
+    setReconciledAmountStyle(recProperties);
+  }, [props]);
+
   return (
     <div className={container}>
       <div className={section}>
@@ -22,15 +60,17 @@ const Totalizer: FC<ITotalizerProps> = (props) => {
       </div>
       <div className={section}>
         <div className={totalizerAmountLabel}>Reconciled Amount</div>
-        <div className={totalizerAmount}>
-          {convertToCurrency(props.reconciledAmount)}
-        </div>{' '}
+        <div
+          className={`${totalizerAmount} ${reconciledAmount}`}
+          style={reconciledAmountStyle as any}
+        ></div>
       </div>
       <div className={section}>
         <div className={totalizerAmountLabel}>Remaining Amount</div>
-        <div className={totalizerAmount}>
-          {convertToCurrency(props.remainingAmount)}
-        </div>{' '}
+        <div
+          className={`${totalizerAmount} ${remainingAmount}`}
+          style={remainingAmountStyle as any}
+        ></div>
       </div>
     </div>
   );
