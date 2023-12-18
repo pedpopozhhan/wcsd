b385ccac752089b9e16f2e9d641d43e15e64b38c
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2023-12-18 13:51:10 -0700

    fixed FWCDS-689

--- src/routes/reconciliation/search-result.ts
+++ src/routes/reconciliation/search-result.ts
@@ -10,7 +10,7 @@ export class SearchResult {
 
 const searchResultColumns: { value: string; label: string }[] = [
   { value: 'vendor', label: 'Vendor' },
-  { value: 'businessId', label: 'Business ID' },
+  { value: 'businessId', label: 'Business No.' },
   { value: 'contractNumber', label: 'Contract No.' },
   { value: 'contractType', label: 'Type' },
   { value: 'numTimeReports', label: 'Time Reports' },
