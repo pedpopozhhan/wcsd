98618fb2d9d026467836c88871890f7732a9f891
Viral.Patel <Viral.Patel@V-VD-PD-0-1402>
2024-01-16 17:09:23 -0500

    replacing gotable with css grid

--- src/routes/process-invoice/tabs/service-sheet-tab.tsx
+++ src/routes/process-invoice/tabs/service-sheet-tab.tsx
@@ -3,12 +3,13 @@ import styles from '../process-invoice.module.scss';
 import { FC, useState } from 'react';
 import { convertToCurrency } from '@/common/currency';
 
-interface IServiceSheetTabProps{
-  InvoiceID: string,
-  InvoiceAmount: number
+interface IServiceSheetTabProps {
+  InvoiceID: string;
+  InvoiceAmount: number;
 }
-const ServiceSheetTab : FC<IServiceSheetTabProps> = (props: IServiceSheetTabProps) => {
-
+const ServiceSheetTab: FC<IServiceSheetTabProps> = (
+  props: IServiceSheetTabProps
+) => {
   let {
     serviceSheetTabContainer,
     serviceSheetTabLabels,
@@ -21,64 +22,42 @@ const ServiceSheetTab : FC<IServiceSheetTabProps> = (props: IServiceSheetTabProp
 
   return (
     <div className={serviceSheetTabContainer}>
-      <GoATable>
-        <tbody>
-          <tr>
-            <td >
-              Unique service sheet name
-            </td>
-            <td>
-              <div>
-                <GoAInput
-                  name='service-sheet-name'
-                  type='text'
-                  onChange={(key, value) => setServiceSheetName(value)}
-                  value={serviceSheetName}
-                />
-              </div>
-              <div className=''>required from Arriba to finish</div>
-            </td>
-          </tr>
-          <tr>
-            <td >Purchase Group</td>
-            <td className={serviceSheetTabAltValues}>W01(FP_W01)</td>
-          </tr>
-          <tr>
-            <td >
-              Service Description
-            </td>
-            <td>Professional Services</td>
-          </tr>
-          <tr>
-            <td >Commodity code</td>
-            <td className={serviceSheetTabAltValues}>
-              [Determined from Contract]
-            </td>
-          </tr>
-          <tr>
-            <td >Material group</td>
-            <td className={serviceSheetTabAltValues}>
-              [Determined from Contract]
-            </td>
-          </tr>
-          <tr>
-            <td >Account type</td>
-            <td className={serviceSheetTabAltValues}>Expense</td>
-          </tr>
-          <tr>
-            <td >Quantity</td>
-            <td className={serviceSheetTabAltValues}>1</td>
-          </tr>
-          <tr>
-            <td >Unit of measure</td>
-            <td className={serviceSheetTabAltValues}>Hour</td>
-          </tr>
-          <tr>
-            <td >Price</td>
-            <td>  {convertToCurrency(props.InvoiceAmount)}</td>
-          </tr>
-        </tbody>
-      </GoATable>
+      <div>Unique service sheet name</div>
+      <div>
+        <GoAInput
+          name='service-sheet-name'
+          type='text'
+          onChange={(key, value) => setServiceSheetName(value)}
+          value={serviceSheetName}
+        />
+        <div className={serviceSheetNameDesc}>
+          required from Arriba to finish
+        </div>
+      </div>
+
+      <div>Purchase group</div>
+      <div className={serviceSheetTabAltValues}>W01(FP_W01)</div>
+
+      <div>Service description</div>
+      <div>Professional Services</div>
+
+      <div>Commodity code</div>
+      <div className={serviceSheetTabAltValues}>[Determined from Contract]</div>
+
+      <div>Material group</div>
+      <div className={serviceSheetTabAltValues}>[Determined from Contract]</div>
+
+      <div>Account type</div>
+      <div className={serviceSheetTabAltValues}>Expense</div>
+
+      <div>Quantity</div>
+      <div className={serviceSheetTabAltValues}>1</div>
+
+      <div>Unit of measure</div>
+      <div className={serviceSheetTabAltValues}>Hour</div>
+
+      <div>Price</div>
+      <div>{convertToCurrency(props.InvoiceAmount)}</div>
     </div>
   );
 };
