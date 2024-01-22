c74894e9a5fd545da8616ecee3ad6a98fd0f80c8
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2024-01-22 12:03:41 -0700

    renamed variables

--- src/interfaces/invoice-details/details-table-row-data.ts
+++ src/interfaces/invoice-details/details-table-row-data.ts
@@ -1,14 +1,14 @@
 export interface IDetailsTableRowData {
-  date: Date;
-  registrationNumber: string;
-  reportNumber: number;
+  flightReportDate: Date;
+  contractRegistrationName: string;
+  flightReportId: number;
   aO02Number: string;
   rateType: string;
   numberOfUnits: number;
   rateUnit: string;
   ratePerUnit: number; //with $0.00
   cost: number; //with $0.00
-  glAccountNumber: number;
+  account: number;
   profitCenter: string;
   costCenter: string;
   fireNumber: string;
