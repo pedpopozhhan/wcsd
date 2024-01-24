b93c7451e05fde2b133b99cc4de28deb58504c2f
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2024-01-23 21:35:46 -0700

    more cleanup

--- src/routes/vendor-time-reports/vendor-time-reports.tsx
+++ src/routes/vendor-time-reports/vendor-time-reports.tsx
@@ -1,42 +1,15 @@
 import { GoAButton, GoATab, GoATabs } from '@abgov/react-components';
-import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
-import React from 'react';
 import styles from './vendor-time-reports.module.scss';
 
-// import { DomainService } from 'report-table-component/src/services/DomainService';
 import SignedOffTabDetails from '../vendor-time-reports/tabs/signed-off-tab-details';
 import ApprovedTabDetails from './tabs/approved-tab-details';
 import VendorTimeReportsSidePanel from '../vendor-time-reports/vendor-time-reports-side-panel';
-// import { FlightReportDashboardService } from '@/services/flight-report-dashboard.service';
 
 const VendorTimeReports = () => {
   const { contractNumber } = useParams();
-  //   (async () => {
-  //     await aviationReportingAuthenticate();
-  //     //  await domainServiceAuthenticate();
-  //   })();
 
-  //   async function aviationReportingAuthenticate() {
-  //     await FlightReportDashboardService.getAuthenticate()
-  //       .then((res) => {
-  //         sessionStorage.setItem('api_token', res.data);
-  //       })
-  //       .catch((err) => {
-  //         console.log('error', err);
-  //       });
-  //   }
-
-  // async function domainServiceAuthenticate() {
-  //   await DomainService.getAuthenticate()
-  //     .then((res) => {
-  //       sessionStorage.setItem('domainService_token', res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log('error', err);
-  //     });
-  // }
   const navigate = useNavigate();
 
   const header = "[Vendor's] Time Reports";
