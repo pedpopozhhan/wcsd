71d02a4393c94fc079319006731585411869fd46
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2024-01-25 10:18:52 -0700

    small changes to slice

--- src/app-slice.ts
+++ src/app-slice.ts
@@ -1,34 +1,27 @@
-// import { createSlice } from '@reduxjs/toolkit';
-// import { IInvoiceData } from './common/invoice-modal-dialog';
+import { createSlice } from '@reduxjs/toolkit';
+import { IInvoiceData } from './common/invoice-modal-dialog';
 
-// export interface IAppSliceState {
-//   timeReportsToReconcile: number[];
-//   invoiceData: IInvoiceData;
-// }
+export interface IAppSliceState {
+  timeReportsToReconcile: number[];
+  invoiceData: IInvoiceData;
+}
 
-// export const appSlice = createSlice({
-//   name: 'counter',
-//   initialState: {
-//     value: { timeReportsToReconcile: [], invoiceData: null },
-//   },
-//   reducers: {
-//     increment: (state) => {
-//       // Redux Toolkit allows us to write "mutating" logic in reducers. It
-//       // doesn't actually mutate the state because it uses the immer library,
-//       // which detects changes to a "draft state" and produces a brand new
-//       // immutable state based off those changes
-//       state.value += 1;
-//     },
-//     decrement: (state) => {
-//       state.value -= 1;
-//     },
-//     incrementByAmount: (state, action) => {
-//       state.value += action.payload;
-//     },
-//   },
-// });
+const initialState: IAppSliceState = {
+  timeReportsToReconcile: [],
+  invoiceData: null as any,
+};
 
-// export const { increment, decrement, incrementByAmount } = appSlice.actions;
+export const appSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    setTimeReportsToReconcile: (state, action) => {
+      state.timeReportsToReconcile += action.payload;
+    },
+  },
+});
 
-// const appReducer = appSlice.reducer;
-// export default appReducer;
+export const { setTimeReportsToReconcile } = appSlice.actions;
+
+const appReducer = appSlice.reducer;
+export default appReducer;
