4f7c3da2d0ca3f526af80a1707b60ca85cca20de
Viral.Patel <Viral.Patel@V-VD-PD-0-1402>
2024-01-29 17:35:58 -0500

    774 - Disabling Process Button Bydefault and moving toast notification in the bottom

--- src/routes/invoice-details/invoice-details.tsx
+++ src/routes/invoice-details/invoice-details.tsx
@@ -41,6 +41,7 @@ export default function InvoiceDetails() {
     setParentShowModal(true);
   };
   const [reconciledAmount, setReconciledAmount] = useState<number>(0);
+  const enableProcess = (invoiceData.InvoiceAmount - reconciledAmount) == 0 ? true : false;
 
   useEffect(() => {
     const subscription = invoiceDetailsService
@@ -145,7 +146,7 @@ export default function InvoiceDetails() {
         </div>
       </div>
       <div className={footer}>
-        <GoAButton type='primary' onClick={processInvoice}>
+        <GoAButton type='primary' onClick={processInvoice} {...(enableProcess ? { disabled: false } : {disabled : true})}>
           Process
         </GoAButton>
         <GoAButton type='secondary' onClick={cancel}>
