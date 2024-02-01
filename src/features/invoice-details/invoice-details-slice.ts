ca450085574af0521ddfeeeb87fe45825b7c46a1
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2024-02-01 14:44:25 -0700

    Commented blocked bugfix

--- src/features/invoice-details/invoice-details-slice.ts
+++ src/features/invoice-details/invoice-details-slice.ts
@@ -15,28 +15,31 @@ const initialState: IInvoiceDetailsSliceState = {
 };
 
 const setRateTypesReducer = (state: IInvoiceDetailsSliceState, action: PayloadAction<string[]>) => {
-  const allowedRateTypes = [
-    'Accommodation',
-    'Airport Fee',
-    'Basing',
-    'Basing Non-Core',
-    'Basing Penalty',
-    'Charter Minimums',
-    'Crew Exp - Breakfast',
-    'Crew Exp - Lunch',
-    'Crew Exp - Dinner',
-    'Crew Expenses',
-    'Double Crew',
-    'Flat',
-    'Fuel',
-    'Landing Fee',
-    'Nav Canada',
-    'Passenger Fee',
-    'Standby',
-    'Vehicle Rental',
-  ];
-  const filtered = action.payload.filter((x) => allowedRateTypes.includes(x));
-  state.rateTypes = filtered;
+  // TODO: this is for the next bug once it is unblocked
+  //   const allowedRateTypes = [
+  //     'Accommodation',
+  //     'Airport Fee',
+  //     'Basing',
+  //     'Basing Non-Core',
+  //     'Basing Penalty',
+  //     'Charter Minimums',
+  //     'Crew Exp - Breakfast',
+  //     'Crew Exp - Lunch',
+  //     'Crew Exp - Dinner',
+  //     'Crew Expenses',
+  //     'Double Crew',
+  //     'Flat',
+  //     'Fuel',
+  //     'Landing Fee',
+  //     'Nav Canada',
+  //     'Passenger Fee',
+  //     'Standby',
+  //     'Vehicle Rental',
+  //   ];
+  //   const filtered = action.payload.filter((x) => allowedRateTypes.includes(x));
+  //   state.rateTypes = filtered;
+
+  state.rateTypes = action.payload;
 };
 
 export const invoiceDetailsSlice = createSlice({
