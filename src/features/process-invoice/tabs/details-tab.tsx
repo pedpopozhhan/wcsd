8eecb0af4fc65b4f4dc465ff65bf912b81f54bdc
Iftikhar Qamar <Iftikhar.Qamar@gov.ab.ca>
2024-02-15 07:44:29 -0700

    details tab from process invoice and view invoice flow

--- src/features/process-invoice/tabs/details-tab.tsx
+++ src/features/process-invoice/tabs/details-tab.tsx
@@ -1,12 +1,34 @@
+import { useState, useEffect, FC } from 'react';
+import styles from '@/features/process-invoice/tabs/details-tab.module.scss';
 
-import { useState } from 'react';
+import { IOtherCostTableRowData } from '@/interfaces/common/other-cost-table-row-data';
+import { ITimeReportDetailsTableRowData } from '@/interfaces/invoice-details/time-report-details-table-row-data';
+import { useAppDispatch, useAppSelector } from '@/app/hooks';
 
-const DetailsTab = () => {
+
+import InvoiceCostDataTable from './invoice-cost-details-table';
+import InvoiceOtherCostTable from './invoice-other-costs-table';
+
+let { container, tabContainer, reconciledDetailsDiv, otherCostsDiv, otherCostHeader } = styles;
+
+interface IReconciledTabProps { }
+
+const InvoiceDetailsTab: FC<IReconciledTabProps> = (props: IReconciledTabProps) => {
+  const otherCostData = useAppSelector((state) => state.processInvoiceTabs.otherCostsData);
+  const costDetailsData = useAppSelector((state) => state.processInvoiceTabs.costDetailsData);
 
   return (
-    <div></div>
-     
+    <div className={container}>
+      <div className={tabContainer}>
+        <div className={reconciledDetailsDiv}>
+          <InvoiceCostDataTable data={costDetailsData} />
+        </div>
+        <div className={otherCostHeader}>Other Costs</div>
+        <div className={otherCostsDiv}>
+          <InvoiceOtherCostTable data={otherCostData} />
+        </div>{' '}
+      </div>
+    </div>
   );
 };
-
-export default DetailsTab;
+export default InvoiceDetailsTab;
