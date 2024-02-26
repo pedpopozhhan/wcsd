65f1a3c5fc0d5540962eaeae2e11dc26aef14dcb
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2024-02-26 15:56:32 -0700

    Wired up platform authentication to front end

--- src/index.tsx
+++ src/index.tsx
@@ -8,25 +8,38 @@ import Contracts from './features/contracts/contracts';
 import VendorTimeReports from './features/vendor-time-reports/vendor-time-reports';
 import ProcessInvoice from './features/process-invoice/process-invoice';
 import ProcessedInvoice from './features/processed-invoice/Processed-invoice';
+import { AuthProvider } from 'react-oidc-context';
+import { oidcConfig } from './common/kc-config';
+import { LoggedOut } from './features/logged-out/logged-out';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' element={<App />}>
-          <Route key='1' path='/' element={<Contracts />} />
-          {/* example usage of protected route 
+  <AuthProvider {...oidcConfig}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <React.StrictMode>
+          <Router>
+            <Routes>
+              <Route path='/' element={<App />}>
+                <Route key='1' path='/' element={<Contracts />} />
+                {/* example usage of protected route 
           <Route element={<ProtectedRoute permissions={[PERMISSION.FIN_INVOICE_V, PERMISSION.FIN_INVOICE_W]} />}>
             <Route path='/reconciliation2' element={<Reconciliation />} />
           </Route>*/}
-          <Route key='2' path='contracts' element={<Contracts />} />
-          <Route key='3' path='VendorTimeReports/:contractNumber' element={<VendorTimeReports />} />
-          <Route key='4' path='invoice/:invoiceId' element={<InvoiceDetails />} />
-          <Route key='5' path='invoice/:invoiceId/processInvoice' element={<ProcessInvoice></ProcessInvoice>} />
-          <Route key='6' path='ProcessedInvoice/:invoiceKey' element={<ProcessedInvoice></ProcessedInvoice>} />
-        </Route>
-      </Routes>
-    </Router>
-  </React.StrictMode>,
+                <Route key='2' path='contracts' element={<Contracts />} />
+                <Route key='3' path='VendorTimeReports/:contractNumber' element={<VendorTimeReports />} />
+                <Route key='4' path='invoice/:invoiceId' element={<InvoiceDetails />} />
+                <Route key='5' path='invoice/:invoiceId/processInvoice' element={<ProcessInvoice></ProcessInvoice>} />
+                <Route key='6' path='ProcessedInvoice/:invoiceKey' element={<ProcessedInvoice></ProcessedInvoice>} />
+                <Route key='7' path='logged-out' element={<LoggedOut />} />
+              </Route>
+            </Routes>
+          </Router>
+        </React.StrictMode>{' '}
+      </PersistGate>
+    </Provider>
+  </AuthProvider>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
