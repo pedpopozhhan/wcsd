2ad9a00dfaa4e05ff41f1f7b10ed189a7822740c
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2024-03-07 13:06:47 -0700

    adding unauthorized page

--- src/app/protectec-route.tsx
+++ src/app/protectec-route.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { PERMISSION } from '@/common/permission';
 import { useConditionalAuth } from './hooks';
+import { hasResourceRole } from '@/common/token-utils';
 
 export interface IProtecedRouteProps {
   permissions: PERMISSION[];
@@ -10,10 +11,13 @@ export interface IProtecedRouteProps {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ProtectedRoute: React.FC<IProtecedRouteProps> = (props) => {
   const auth = useConditionalAuth();
-  if (!auth.isAuthenticated) {
+  const hasPermissions = props.permissions.some((x) => {
+    return hasResourceRole('finance', x, auth?.user?.access_token);
+  });
+  if (!auth.isAuthenticated || !hasPermissions) {
     // also check permissions in future if needed
     console.error('Unauthorized');
-    return <Navigate to='logged-out' replace />;
+    return <Navigate to='unauthorized' replace />;
   }
 
   return <Outlet />;
