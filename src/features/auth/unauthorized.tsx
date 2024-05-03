bfd9e8f4551e580369e591e15be55ba41eb418f4
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2024-05-03 14:12:06 -0600

    fwcds1279 added email link

--- src/features/auth/unauthorized.tsx
+++ src/features/auth/unauthorized.tsx
@@ -8,7 +8,7 @@ export const UnAuthorized = () => {
         <span>
           You are currently unable to access any applications in the portal because no user roles are assigned to you. For access, please contact one
           of the following: your supervisor, your Area&apos;s Regional Administrator, your Wildfire Management Branch contact, or Wildfire
-          Applications Support via wildfire.applications@gov.ab.ca or +1 780-427-8100
+          Applications Support via <a href='mailto:wildfire.applications@gov.ab.ca'>wildfire.applications@gov.ab.ca</a> or +1 780-427-8100
         </span>
       </div>
     </>
