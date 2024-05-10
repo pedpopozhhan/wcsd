16d6ead6cab6b7eca1c96812570fbf15450a71ea
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2024-05-10 09:21:12 -0600

    fwcds1308 added created by field to ui instead of assigne to

--- src/interfaces/processed-invoice/processed-invoice-data.ts
+++ src/interfaces/processed-invoice/processed-invoice-data.ts
@@ -2,25 +2,26 @@ import { ITimeReportDetailsTableRowData } from '@/interfaces/invoice-details/tim
 import { IOtherCostTableRowData } from '@/interfaces/common/other-cost-table-row-data';
 
 export interface IProcessedInvoiceData {
-    invoiceId?: string;
-    invoiceNumber: string;
-    invoiceDate: Date;
-    invoiceAmount: number;
-    periodEndDate: Date;
-    invoiceReceivedDate: Date;
-    vendor: string;
-    assignedTo: string;
-    contractNumber: string;
-    type: string;
-    invoiceTimeReportCostDetails: ITimeReportDetailsTableRowData[];
-    invoiceOtherCostDetails: IOtherCostTableRowData[];
-    uniqueServiceSheetName: string,
-    purchaseGroup: string,
-    serviceDescription: string,
-    communityCode: string,
-    materialGroup: string,
-    accountType: string,
-    quantity: number,
-    unitOfMeasure: string,
-    price: number,
+  invoiceId?: string;
+  invoiceNumber: string;
+  invoiceDate: Date;
+  invoiceAmount: number;
+  periodEndDate: Date;
+  invoiceReceivedDate: Date;
+  vendor: string;
+  assignedTo: string;
+  contractNumber: string;
+  type: string;
+  invoiceTimeReportCostDetails: ITimeReportDetailsTableRowData[];
+  invoiceOtherCostDetails: IOtherCostTableRowData[];
+  uniqueServiceSheetName: string;
+  purchaseGroup: string;
+  serviceDescription: string;
+  communityCode: string;
+  materialGroup: string;
+  accountType: string;
+  quantity: number;
+  unitOfMeasure: string;
+  price: number;
+  createdBy: string;
 }
