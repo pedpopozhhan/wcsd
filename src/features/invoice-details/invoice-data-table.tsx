4c9ccc07e77e6d7936618a029344c9dffcee1108
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2024-09-04 12:44:19 -0600

    fwcds1834 fixed filtering on payables tab

--- src/features/invoice-details/invoice-data-table.tsx
+++ src/features/invoice-details/invoice-data-table.tsx
@@ -6,6 +6,7 @@ import { IDetailsTableRow } from './details-table-row.interface';
 import { useAppDispatch, useAppSelector } from '@/app/hooks';
 import { ITimeReportDetailsTableRowData, getFireNumberRow } from '@/interfaces/invoice-details/time-report-details-table-row-data';
 import { setRowData } from '@/app/app-slice';
+import { useEffect, useState } from 'react';
 
 const { container, checkboxWrapper, buttonWrapper, tableContainer, stickyColumn, start, end, onTop, totalRowLabel, totalRowValue, number } = styles;
 interface IDetailsTabProps {
@@ -16,13 +17,21 @@ interface IDetailsTabProps {
 }
 const InvoiceDataTable: React.FC<IDetailsTabProps> = (props) => {
   const dispatch = useAppDispatch();
-  const rowData = useAppSelector((state) => state.app.rowData);
+  const rawData = useAppSelector((state) => state.app.rowData);
   const filterByRateType = (x: IDetailsTableRow) => {
-    return props.rateTypeFilter && props.rateTypeFilter.length !== 0 ? props.rateTypeFilter.includes(x.data.rateType) : x; // === props.rateTypeFilter : x;
+    return props.rateTypeFilter && props.rateTypeFilter.length !== 0 ? props.rateTypeFilter.includes(x.data.rateType) : true; // === props.rateTypeFilter : x;
   };
+  const [sortBy, setSortBy] = useState<string>('flightReportDate');
+  const [sortDir, setSortDir] = useState<number>(-1);
+  const [tableData, setTableData] = useState<IDetailsTableRow[]>([]);
+  useEffect(() => {
+    sortData(sortBy, sortDir);
+  }, [JSON.stringify(props.rateTypeFilter)]);
 
   function sortData(sortBy: string, sortDir: number) {
-    const data = [...rowData];
+    setSortDir(sortDir);
+    setSortBy(sortBy);
+    const data = [...rawData];
     const sorted = data.filter(filterByRateType).sort((a: IDetailsTableRow, b: IDetailsTableRow) => {
       const varA = a.data[sortBy as keyof ITimeReportDetailsTableRowData];
       const varB = b.data[sortBy as keyof ITimeReportDetailsTableRowData];
@@ -35,14 +44,15 @@ const InvoiceDataTable: React.FC<IDetailsTabProps> = (props) => {
       }
       return (varA > varB ? 1 : -1) * sortDir;
     });
-    dispatch(setRowData(sorted));
+    setTableData(sorted);
+    // dispatch(setRowData(sorted));
   }
   function addRemoveClicked(row: IDetailsTableRow) {
     const isAdd = !row.isAdded;
 
     dispatch(
       setRowData(
-        rowData.map((r) => {
+        rawData.map((r) => {
           if (r.index === row.index) {
             return { ...r, isAdded: isAdd };
           } else {
@@ -55,7 +65,7 @@ const InvoiceDataTable: React.FC<IDetailsTabProps> = (props) => {
   function checkClicked(row: IDetailsTableRow, checked: boolean) {
     dispatch(
       setRowData(
-        rowData.map((r) => {
+        rawData.map((r) => {
           if (r.index === row.index) {
             return { ...r, isSelected: checked };
           } else {
@@ -67,20 +77,20 @@ const InvoiceDataTable: React.FC<IDetailsTabProps> = (props) => {
   }
 
   function checkAll() {
-    const filteredRecords = rowData?.filter(filterByRateType).filter(getFilter());
-    if (filteredRecords.length === rowData.length) {
-      const allSelected = rowData.filter((x) => !x.isAdded).every((x) => x.isSelected);
+    const filteredRecords = rawData?.filter(filterByRateType).filter(getFilter());
+    if (filteredRecords.length === rawData.length) {
+      const allSelected = rawData.filter((x) => !x.isAdded).every((x) => x.isSelected);
       dispatch(
         setRowData(
-          rowData.map((r) => {
+          rawData.map((r) => {
             return r.isAdded ? r : { ...r, isSelected: allSelected ? false : true };
           }),
         ),
       );
-    } else if (filteredRecords.length < rowData?.length) {
+    } else if (filteredRecords.length < rawData?.length) {
       dispatch(
         setRowData(
-          rowData.map((r) => {
+          rawData.map((r) => {
             const exists = filteredRecords.some((obj) => obj === r);
             return r.isAdded ? r : { ...r, isSelected: exists && !r.isSelected ? true : false };
           }),
@@ -104,8 +114,8 @@ const InvoiceDataTable: React.FC<IDetailsTabProps> = (props) => {
                   <div className={checkboxWrapper}>
                     <GoACheckbox
                       name={''}
-                      checked={rowData.filter((x) => !x.isAdded).every((x) => x.isSelected)}
-                      disabled={rowData.every((x) => x.isAdded)}
+                      checked={rawData.filter((x) => !x.isAdded).every((x) => x.isSelected)}
+                      disabled={rawData.every((x) => x.isAdded)}
                       onChange={() => checkAll()}
                     ></GoACheckbox>
                   </div>
@@ -138,50 +148,50 @@ const InvoiceDataTable: React.FC<IDetailsTabProps> = (props) => {
             </tr>
           </thead>
           <tbody>
-            {rowData
+            {/* {rawData
               .filter(filterByRateType)
-              .filter(getFilter())
-              .map((x, index) => (
-                <tr key={index}>
-                  {props.showCheckBoxes && (
-                    <td className={`${stickyColumn} ${start}`}>
-                      <div className={checkboxWrapper}>
-                        <GoACheckbox
-                          name={`cb${index}`}
-                          checked={x.isSelected}
-                          disabled={x.isAdded ? true : false}
-                          onChange={(name, checked) => {
-                            checkClicked(x, checked);
-                          }}
-                        ></GoACheckbox>
-                      </div>
-                    </td>
-                  )}
-                  {props.showRowIndicator && <td>{index + 1}</td>}
-                  <td>{yearMonthDay(x.data.flightReportDate)}</td>
-                  <td>{x.data.contractRegistrationName}</td>
-                  <td>{x.data.flightReportId}</td>
-                  <td>{x.data.ao02Number}</td>
-                  <td>{x.data.rateType}</td>
-                  <td className={number}>{x.data.noOfUnits.toFixed(3)}</td>
-                  <td>{x.data.rateUnit}</td>
-                  <td>{convertToCurrency(x.data.ratePerUnit)}</td>
-                  <td>{convertToCurrency(x.data.cost)}</td>
-                  <td>{x.data.internalOrder}</td>
-                  <td>{x.data.costCenter}</td>
-                  <td>{x.data.fund}</td>
-                  <td>{x.data.account}</td>
-                  <td>{getFireNumberRow(x.data)}</td>
-                  <td className={`${stickyColumn} ${end}`}>
-                    <div className={buttonWrapper}>
-                      <GoAButton size='compact' type='secondary' disabled={rowData.some((x) => x.isSelected)} onClick={() => addRemoveClicked(x)}>
-                        {x.isAdded ? 'Remove' : 'Add'}
-                      </GoAButton>
+              .filter(getFilter()) */}
+            {tableData.map((x, index) => (
+              <tr key={index}>
+                {props.showCheckBoxes && (
+                  <td className={`${stickyColumn} ${start}`}>
+                    <div className={checkboxWrapper}>
+                      <GoACheckbox
+                        name={`cb${index}`}
+                        checked={x.isSelected}
+                        disabled={x.isAdded ? true : false}
+                        onChange={(name, checked) => {
+                          checkClicked(x, checked);
+                        }}
+                      ></GoACheckbox>
                     </div>
                   </td>
-                </tr>
-              ))}
-            <tr key={rowData?.length + 1}>
+                )}
+                {props.showRowIndicator && <td>{index + 1}</td>}
+                <td>{yearMonthDay(x.data.flightReportDate)}</td>
+                <td>{x.data.contractRegistrationName}</td>
+                <td>{x.data.flightReportId}</td>
+                <td>{x.data.ao02Number}</td>
+                <td>{x.data.rateType}</td>
+                <td className={number}>{x.data.noOfUnits.toFixed(3)}</td>
+                <td>{x.data.rateUnit}</td>
+                <td>{convertToCurrency(x.data.ratePerUnit)}</td>
+                <td>{convertToCurrency(x.data.cost)}</td>
+                <td>{x.data.internalOrder}</td>
+                <td>{x.data.costCenter}</td>
+                <td>{x.data.fund}</td>
+                <td>{x.data.account}</td>
+                <td>{getFireNumberRow(x.data)}</td>
+                <td className={`${stickyColumn} ${end}`}>
+                  <div className={buttonWrapper}>
+                    <GoAButton size='compact' type='secondary' disabled={rawData.some((x) => x.isSelected)} onClick={() => addRemoveClicked(x)}>
+                      {x.isAdded ? 'Remove' : 'Add'}
+                    </GoAButton>
+                  </div>
+                </td>
+              </tr>
+            ))}
+            <tr key={rawData?.length + 1}>
               <td></td>
               <td></td>
               <td></td>
@@ -195,11 +205,10 @@ const InvoiceDataTable: React.FC<IDetailsTabProps> = (props) => {
               </td>
               <td>
                 <div className={`${totalRowValue} ${number}`}>
-                  {rowData
+                  {/* {rawData
                     ?.filter(filterByRateType)
-                    .filter(getFilter())
-                    .reduce((unit, obj) => unit + obj.data.noOfUnits, 0)
-                    .toFixed(3)}
+                    .filter(getFilter()) */}
+                  {tableData.reduce((unit, obj) => unit + obj.data.noOfUnits, 0).toFixed(3)}
                 </div>
               </td>
               <td></td>
@@ -209,10 +218,10 @@ const InvoiceDataTable: React.FC<IDetailsTabProps> = (props) => {
               <td>
                 <div className={totalRowValue}>
                   {convertToCurrency(
-                    rowData
-                      ?.filter(filterByRateType)
-                      .filter(getFilter())
-                      .reduce((cost, obj) => cost + obj.data.cost, 0),
+                    // rawData
+                    //   ?.filter(filterByRateType)
+                    //   .filter(getFilter())
+                    tableData.reduce((cost, obj) => cost + obj.data.cost, 0),
                   )}
                 </div>
               </td>
